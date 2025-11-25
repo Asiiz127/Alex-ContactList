d394302a83e36d06a3d8a4f849e22bb4ffe8fb13
Asiiz127 <alexmartintv@gmail.com>
2025-11-25 14:02:54 +0100

    cambios en routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,23 +1,10 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import ContactList from "./pages/ContactList";
 import CreateContact from "./pages/CreateContact";
 import EditContact from "./pages/EditContact";
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <ContactList />,
-  },
-  {
-    path: "/createContact",
-    element: <CreateContact />,
-  },
-  {
-    path: "/editContact/:id",
-    element: <EditContact />,
-  }
+  { path: "/", element: <ContactList />, },
+  { path: "/createContact", element: <CreateContact />, },
+  { path: "/editContact/:id", element: <EditContact />, }
 ]);
